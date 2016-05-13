458af4c22cabd6593f46ff83f22de3e4ffd8c206
Robert Herdzik <robert.herdzik@yahoo.com>
2016-05-13 22:25:09 +0200

    Update to Swift 2.2

--- Pod/Classes/LiquittableCircle.swift
+++ Pod/Classes/LiquittableCircle.swift
@@ -32,7 +32,7 @@ class LiquittableCircle : UIView {
         setup()
     }
 
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
